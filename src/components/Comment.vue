fe6b2ff396a5b2c9530e9f2e371d654fb35c10f1
eeny <59357089+eeny@users.noreply.github.com>
2022-02-07 22:43:59 +0900

    vue-router nested routes, 페이지 이동관련 기능

---
+++ src/components/Comment.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>댓글입니다.</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -4,6 +4,7 @@
     <h5>{{ blogContents[$route.params.id].title }}</h5>
     <p>{{ blogContents[$route.params.id].content }}</p>
     <p>{{ blogContents[$route.params.id].date }}</p>
+    <router-view></router-view>
   </div>
 </template>
 
